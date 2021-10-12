ff3890248c76f2a4cffce7898932668bc3816d6c
liuhu <1321947437@qq.com>
2021-10-12 23:05:30 +0800

    demo2

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,5 +1,7 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("hello");
+        demo2 demo2=new demo2();
+        demo2.cesi();
     }
 }
---
+++ src/main/java/demo2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class demo2 {
+    public  void demo2(){
+        System.out.println("开始测试了");
+    }
+    public void cesi(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("please you print to name");
+        String name=scanner.nextLine();
+        System.out.println("you name"+name);
+
+    }
+}
