1519fa1732156016019033f6931fd0984696cfa3
liuhu <1321947437@qq.com>
2021-10-15 23:18:35 +0800

    demo3

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,10 +1,21 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
 public class demo {
     public static void main(String[] args) {
         System.out.println("hello");
-        demo2 demo2=new demo2();
-       System.out.println(demo2.getClass());
-        demo2.cesi();
-        demo2.datedemo();
-        jframdemo.xihuan();
+//        demo2 demo2=new demo2();
+//       System.out.println(demo2.getClass());
+//        demo2.cesi();
+//        demo2.datedemo();
+//        jframdemo.xihuan();
+        ActionListener listener = new TimePrinter();
+        Timer t = new Timer(10000000, listener);
+        t.start();
+
+        JOptionPane.showMessageDialog(null, "QUit program");
+        System.exit(0);
+
+
     }
 }
